2a62de7f0c52873b113139d151eb15596a1fe76b
Alfredo <21030761@itcelaya.edu.mx>
2023-06-16 10:32:18 -0600

    Method to get Recipes finished

--- src/demo.js
+++ src/demo.js
@@ -1,32 +1,23 @@
-const urlAPI = 'https://pizza-and-desserts.p.rapidapi.com/pizzas/1';
-//const apiView = document.getElementById('viewContainer');
+//Get recipes
+const recipeSAPI = 'https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=';
+let recipe_v = 'meat%20balls';
 
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '532d0c1310msh988dee28f976e8ep12bc49jsn1b74a5f379a5',
-		'X-RapidAPI-Host': 'pizza-and-desserts.p.rapidapi.com'
+		'X-RapidAPI-Host': 'recipe-by-api-ninjas.p.rapidapi.com'
 	}
 };
 
-async function consumeAPI(urlAPI){
-    const response = await fetch(urlAPI, options);
-    const data = response.text();
+async function getRecipe(url, recipe){
+    const response = await fetch(`${url}${recipe}`, options);
+    const data = await response.json();
     return data;
 }
 
-consumeAPI(urlAPI)
-    .then(response => {console.log('Response: '+response);})
-    .catch(error => {console.log('Error: '+error);});
+getRecipe(recipeSAPI, recipe_v)
+    .then(response => console.log(response))
+    .catch(error => console.log('Error: '+error));
 
 
-    const url = 'https://pizza-and-desserts.p.rapidapi.com/pizzas/1';
-    
-    
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-    }
