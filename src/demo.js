632e9789e6df9f4a238a053d0a1ff0929b38243d
Alfredo <21030761@itcelaya.edu.mx>
2023-06-16 10:43:16 -0600

    Method to get Nutritional info finished

--- src/demo.js
+++ src/demo.js
@@ -1,23 +1,39 @@
 //Get recipes
 const recipeSAPI = 'https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=';
-let recipe_v = 'meat%20balls';
+const nutritionalAPI = 'https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=';
 
-const options = {
+const recipeOptions = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '532d0c1310msh988dee28f976e8ep12bc49jsn1b74a5f379a5',
 		'X-RapidAPI-Host': 'recipe-by-api-ninjas.p.rapidapi.com'
 	}
 };
+const nutritionalOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '532d0c1310msh988dee28f976e8ep12bc49jsn1b74a5f379a5',
+		'X-RapidAPI-Host': 'nutrition-by-api-ninjas.p.rapidapi.com'
+	}
+};
 
 async function getRecipe(url, recipe){
-    const response = await fetch(`${url}${recipe}`, options);
+    const response = await fetch(`${url}${recipe}`, recipeOptions);
     const data = await response.json();
     return data;
 }
 
-getRecipe(recipeSAPI, recipe_v)
+async function getNutritional_info(url, food) {
+    const response = await fetch(`${url}${food}`, nutritionalOptions);
+    const data = await response.json();
+    return data;
+}
+
+// getRecipe(recipeSAPI, 'meat%20balls')
+//     .then(response => console.log(response))
+//     .catch(error => console.log('Error: '+error));
+
+getNutritional_info(nutritionalAPI, 'pepperoni%20pizza')
     .then(response => console.log(response))
     .catch(error => console.log('Error: '+error));
-
 
