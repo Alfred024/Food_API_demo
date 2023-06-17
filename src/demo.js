2d099ab35cb8e8d9bf80ec57fa3d4d44994ff873
Alfredo <21030761@itcelaya.edu.mx>
2023-06-16 21:43:20 -0600

    First part of searchFood method (get objects parsed and value of food seacrh)

--- src/demo.js
+++ src/demo.js
@@ -31,18 +31,57 @@ async function getRecipe(url, recipe){
     const data = await response.json();
     return data;
 }
-
 async function getNutritional_info(url, food) {
     const response = await fetch(`${url}${food}`, nutritionalOptions);
     const data = await response.json();
     return data;
 }
-
 async function getImgs(url, img) {
     const response = await fetch(`${url}${img}`, imgsOptions);
     const data = await response.json();
     return data;
 }
+
+async function searchFood() {
+    
+    // Convierte la búsqueda del input en un formato para la API
+    // let recipeSearch = fillSearch();
+    // Objeto JSON con las recetas
+    // const recipes = await getRecipe(recipeSAPI, recipeSearch);
+
+    const recipesSection = document.getElementById('recipes_container');
+    const imgResource = await getImgs(imgsAPI, recipeSearch);
+
+    recipes.forEach(element => {
+        const card = document.createElement('article');
+        const title = document.createElement('p');
+        const btnInfo = document.createElement('button');
+        const imgView = document.createElement('img');
+
+        title.innerHTML = element.title;
+        imgView.src = imgResource.data[0].url;
+        btnInfo.innerHTML = "Info:";
+
+        recipesSection.appendChild(card);
+        card.appendChild(btnInfo)
+        card.appendChild(imgView);
+    });
+}
+
+function fillSearch() {
+    let recipeArray =  document.getElementById('recipe_input').value;
+    let nameRecipe = '';
+    for (let index = 0; index < recipeArray.length; index++) {
+        if(recipeArray.charAt(index) === ' '){
+            nameRecipe += '%20';
+        }else{
+            nameRecipe += recipeArray.charAt(index);
+        }
+    }
+    return nameRecipe;
+}
+
+searchFood();
 
 // getRecipe(recipeSAPI, 'meat%20balls')
 //     .then(response => console.log(response))
@@ -52,8 +91,8 @@ async function getImgs(url, img) {
 //     .then(response => console.log(response))
 //     .catch(error => console.log('Error: '+error));
 
-getImgs(imgsAPI, 'pepperoni%20pizza')
-    .then(response => console.log(response))
-    .catch(error => console.log('Error: '+error));
+// getImgs(imgsAPI, 'pepperoni%20pizza')
+//     .then(response => console.log(response))
+//     .catch(error => console.log('Error: '+error));
 
 
